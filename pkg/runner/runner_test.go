57569c4fac72ba6ab331c300a16076c68901a1d0
Ryan <me@hackerc.at>
2022-03-30 17:54:44 +0200

    tests(runner): move arm64 test to own func
    Prevents random failing of all other runs when qemu binfmt
    is not set up
    
    Signed-off-by: Ryan <me@hackerc.at>

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -16,11 +16,25 @@ import (
 	"github.com/nektos/act/pkg/model"
 )
 
-var baseImage = "node:16-buster-slim"
+var (
+	baseImage = "node:16-buster-slim"
+	platforms map[string]string
+)
 
 func init() {
 	if p := os.Getenv("ACT_TEST_IMAGE"); p != "" {
 		baseImage = p
+	}
+
+	platforms = map[string]string{
+		"ubuntu-latest": baseImage,
+	}
+
+	log.SetLevel(log.DebugLevel)
+	if l := os.Getenv("ACT_TEST_LOG_LEVEL"); l != "" {
+		if lvl, err := log.ParseLevel(l); err == nil {
+			log.SetLevel(lvl)
+		}
 	}
 }
 
@@ -43,12 +57,11 @@ func TestGraphEvent(t *testing.T) {
 }
 
 type TestJobFileInfo struct {
-	workdir               string
-	workflowPath          string
-	eventName             string
-	errorMessage          string
-	platforms             map[string]string
-	containerArchitecture string
+	workdir      string
+	workflowPath string
+	eventName    string
+	errorMessage string
+	platforms    map[string]string
 }
 
 func runTestJobFile(ctx context.Context, t *testing.T, tjfi TestJobFileInfo) {
@@ -57,13 +70,12 @@ func runTestJobFile(ctx context.Context, t *testing.T, tjfi TestJobFileInfo) {
 		assert.Nil(t, err, workdir)
 		fullWorkflowPath := filepath.Join(workdir, tjfi.workflowPath)
 		runnerConfig := &Config{
-			Workdir:               workdir,
-			BindWorkdir:           false,
-			EventName:             tjfi.eventName,
-			Platforms:             tjfi.platforms,
-			ReuseContainers:       false,
-			ContainerArchitecture: tjfi.containerArchitecture,
-			GitHubInstance:        "github.com",
+			Workdir:         workdir,
+			BindWorkdir:     false,
+			EventName:       tjfi.eventName,
+			Platforms:       tjfi.platforms,
+			ReuseContainers: false,
+			GitHubInstance:  "github.com",
 		}
 
 		runner, err := New(runnerConfig)
@@ -88,69 +100,91 @@ func TestRunEvent(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	platforms := map[string]string{
-		"ubuntu-latest": baseImage,
-	}
+	ctx := context.Background()
 
 	tables := []TestJobFileInfo{
-		{"testdata", "basic", "push", "", platforms, ""},
-		{"testdata", "fail", "push", "exit with `FAILURE`: 1", platforms, ""},
-		{"testdata", "runs-on", "push", "", platforms, ""},
-		{"testdata", "checkout", "push", "", platforms, ""},
-		{"testdata", "non-existent-action", "push", "Job 'nopanic' failed", platforms, ""},
-		{"testdata", "shells/defaults", "push", "", platforms, ""},
+		{"testdata", "basic", "push", "", platforms},
+		{"testdata", "fail", "push", "exit with `FAILURE`: 1", platforms},
+		{"testdata", "runs-on", "push", "", platforms},
+		{"testdata", "checkout", "push", "", platforms},
+		{"testdata", "non-existent-action", "push", "Job 'nopanic' failed", platforms},
+		{"testdata", "shells/defaults", "push", "", platforms},
 		// TODO: figure out why it fails
-		// {"testdata", "shells/custom", "push", "", map[string]string{"ubuntu-latest": "ghcr.io/justingrote/act-pwsh:latest"}, ""}, // custom image with pwsh
-		{"testdata", "shells/pwsh", "push", "", map[string]string{"ubuntu-latest": "ghcr.io/justingrote/act-pwsh:latest"}, ""}, // custom image with pwsh
-		{"testdata", "shells/bash", "push", "", platforms, ""},
-		{"testdata", "shells/python", "push", "", map[string]string{"ubuntu-latest": "node:16-buster"}, ""}, // slim doesn't have python
-		{"testdata", "shells/sh", "push", "", platforms, ""},
-		{"testdata", "job-container", "push", "", platforms, ""},
-		{"testdata", "job-container-non-root", "push", "", platforms, ""},
-		{"testdata", "container-hostname", "push", "", platforms, ""},
-		{"testdata", "uses-docker-url", "push", "", platforms, ""},
-		{"testdata", "remote-action-docker", "push", "", platforms, ""},
-		{"testdata", "remote-action-js", "push", "", platforms, ""},
-		{"testdata", "local-action-docker-url", "push", "", platforms, ""},
-		{"testdata", "local-action-dockerfile", "push", "", platforms, ""},
-		{"testdata", "local-action-via-composite-dockerfile", "push", "", platforms, ""},
-		{"testdata", "local-action-js", "push", "", platforms, ""},
-		{"testdata", "matrix", "push", "", platforms, ""},
-		{"testdata", "matrix-include-exclude", "push", "", platforms, ""},
-		{"testdata", "commands", "push", "", platforms, ""},
-		{"testdata", "workdir", "push", "", platforms, ""},
-		{"testdata", "defaults-run", "push", "", platforms, ""},
-		{"testdata", "uses-composite", "push", "", platforms, ""},
-		{"testdata", "uses-composite-with-error", "push", "Job 'failing-composite-action' failed", platforms, ""},
-		{"testdata", "uses-nested-composite", "push", "", platforms, ""},
-		{"testdata", "composite-fail-with-output", "push", "", platforms, ""},
-		{"testdata", "issue-597", "push", "", platforms, ""},
-		{"testdata", "issue-598", "push", "", platforms, ""},
-		{"testdata", "if-env-act", "push", "", platforms, ""},
-		{"testdata", "env-and-path", "push", "", platforms, ""},
-		{"testdata", "outputs", "push", "", platforms, ""},
-		{"testdata", "steps-context/conclusion", "push", "", platforms, ""},
-		{"testdata", "steps-context/outcome", "push", "", platforms, ""},
-		{"testdata", "job-status-check", "push", "job 'fail' failed", platforms, ""},
-		{"testdata", "if-expressions", "push", "Job 'mytest' failed", platforms, ""},
-		{"testdata", "evalmatrix", "push", "", platforms, ""},
-		{"testdata", "evalmatrixneeds", "push", "", platforms, ""},
-		{"testdata", "evalmatrixneeds2", "push", "", platforms, ""},
-		{"testdata", "evalmatrix-merge-map", "push", "", platforms, ""},
-		{"testdata", "evalmatrix-merge-array", "push", "", platforms, ""},
-		{"../model/testdata", "strategy", "push", "", platforms, ""}, // TODO: move all testdata into pkg so we can validate it with planner and runner
-		// {"testdata", "issue-228", "push", "", platforms, ""}, // TODO [igni]: Remove this once everything passes
-
-		// single test for different architecture: linux/arm64
-		{"testdata", "basic", "push", "", platforms, "linux/arm64"},
+		// {"testdata", "shells/custom", "push", "", map[string]string{"ubuntu-latest": "ghcr.io/justingrote/act-pwsh:latest"}}, // custom image with pwsh
+		{"testdata", "shells/pwsh", "push", "", map[string]string{"ubuntu-latest": "ghcr.io/justingrote/act-pwsh:latest"}}, // custom image with pwsh
+		{"testdata", "shells/bash", "push", "", platforms},
+		{"testdata", "shells/python", "push", "", map[string]string{"ubuntu-latest": "node:16-buster"}}, // slim doesn't have python
+		{"testdata", "shells/sh", "push", "", platforms},
+		{"testdata", "job-container", "push", "", platforms},
+		{"testdata", "job-container-non-root", "push", "", platforms},
+		{"testdata", "container-hostname", "push", "", platforms},
+		{"testdata", "uses-docker-url", "push", "", platforms},
+		{"testdata", "remote-action-docker", "push", "", platforms},
+		{"testdata", "remote-action-js", "push", "", platforms},
+		{"testdata", "local-action-docker-url", "push", "", platforms},
+		{"testdata", "local-action-dockerfile", "push", "", platforms},
+		{"testdata", "local-action-via-composite-dockerfile", "push", "", platforms},
+		{"testdata", "local-action-js", "push", "", platforms},
+		{"testdata", "matrix", "push", "", platforms},
+		{"testdata", "matrix-include-exclude", "push", "", platforms},
+		{"testdata", "commands", "push", "", platforms},
+		{"testdata", "workdir", "push", "", platforms},
+		{"testdata", "defaults-run", "push", "", platforms},
+		{"testdata", "uses-composite", "push", "", platforms},
+		{"testdata", "uses-composite-with-error", "push", "Job 'failing-composite-action' failed", platforms},
+		{"testdata", "uses-nested-composite", "push", "", platforms},
+		{"testdata", "composite-fail-with-output", "push", "", platforms},
+		{"testdata", "issue-597", "push", "", platforms},
+		{"testdata", "issue-598", "push", "", platforms},
+		{"testdata", "if-env-act", "push", "", platforms},
+		{"testdata", "env-and-path", "push", "", platforms},
+		{"testdata", "outputs", "push", "", platforms},
+		{"testdata", "steps-context/conclusion", "push", "", platforms},
+		{"testdata", "steps-context/outcome", "push", "", platforms},
+		{"testdata", "job-status-check", "push", "job 'fail' failed", platforms},
+		{"testdata", "if-expressions", "push", "Job 'mytest' failed", platforms},
+		{"testdata", "evalmatrix", "push", "", platforms},
+		{"testdata", "evalmatrixneeds", "push", "", platforms},
+		{"testdata", "evalmatrixneeds2", "push", "", platforms},
+		{"testdata", "evalmatrix-merge-map", "push", "", platforms},
+		{"testdata", "evalmatrix-merge-array", "push", "", platforms},
+		{"../model/testdata", "strategy", "push", "", platforms}, // TODO: move all testdata into pkg so we can validate it with planner and runner
+		// {"testdata", "issue-228", "push", "", platforms}, // TODO [igni]: Remove this once everything passes
 	}
-	log.SetLevel(log.DebugLevel)
-
-	ctx := context.Background()
 
 	for _, table := range tables {
 		runTestJobFile(ctx, t, table)
 	}
+}
+
+func TestRunDifferentArchitecture(t *testing.T) {
+	if testing.Short() {
+		t.Skip("skipping integration test")
+	}
+
+	ctx := context.Background()
+
+	tjfi := TestJobFileInfo{"testdata", "basic", "push", "", platforms}
+	workdir, err := filepath.Abs(tjfi.workdir)
+	assert.Nil(t, err, workdir)
+	fullWorkflowPath := filepath.Join(workdir, tjfi.workflowPath)
+	runnerConfig := &Config{
+		Workdir:               workdir,
+		EventName:             tjfi.eventName,
+		Platforms:             tjfi.platforms,
+		ContainerArchitecture: "linux/arm64",
+	}
+
+	runner, err := New(runnerConfig)
+	assert.Nil(t, err, tjfi.workflowPath)
+
+	planner, err := model.NewWorkflowPlanner(fullWorkflowPath, true)
+	assert.Nil(t, err, fullWorkflowPath)
+
+	plan := planner.PlanEvent(tjfi.eventName)
+
+	err = runner.NewPlanExecutor(plan)(ctx)
+	assert.Nil(t, err, fullWorkflowPath)
 }
 
 func TestRunEventSecrets(t *testing.T) {
@@ -158,12 +192,7 @@ func TestRunEventSecrets(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	log.SetLevel(log.DebugLevel)
 	ctx := context.Background()
-
-	platforms := map[string]string{
-		"ubuntu-latest": baseImage,
-	}
 
 	workflowPath := "secrets"
 	eventName := "push"
@@ -199,12 +228,7 @@ func TestRunEventPullRequest(t *testing.T) {
 		t.Skip("skipping integration test")
 	}
 
-	log.SetLevel(log.DebugLevel)
 	ctx := context.Background()
-
-	platforms := map[string]string{
-		"ubuntu-latest": baseImage,
-	}
 
 	workflowPath := "pull-request"
 	eventName := "pull_request"
