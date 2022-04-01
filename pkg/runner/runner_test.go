14f586ea3d75c2ec7e7462695807047869b5dbbf
Markus Wolf <mail@markus-wolf.de>
2022-04-01 11:26:27 +0200

    test: add case for env and context

--- pkg/runner/runner_test.go
+++ pkg/runner/runner_test.go
@@ -138,6 +138,7 @@ func TestRunEvent(t *testing.T) {
 		{"testdata", "evalmatrixneeds2", "push", "", platforms, ""},
 		{"testdata", "evalmatrix-merge-map", "push", "", platforms, ""},
 		{"testdata", "evalmatrix-merge-array", "push", "", platforms, ""},
+		{"testdata", "actions-environment-and-context-tests", "push", "", platforms, ""},
 		{"../model/testdata", "strategy", "push", "", platforms, ""}, // TODO: move all testdata into pkg so we can validate it with planner and runner
 		// {"testdata", "issue-228", "push", "", platforms, ""}, // TODO [igni]: Remove this once everything passes
 
